fd9bd3f4f0c8c2743da84e0bc29760c4c149f2f8
Laurence Burt <laurencemb@hotmail.co.uk>
2023-07-03 16:28:43 +0100

    added socialsto footer

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom';
 import Navlink from '../Nav-Link/navlink.js';
 import '../../global.css';
 import CenterContainer from "../CenterAlignContainer/CenterContainer";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons';
+import { faBehanceSquare } from '@fortawesome/free-brands-svg-icons';
 
 
 function Footer(props) {
@@ -29,16 +33,26 @@ function Footer(props) {
                                 {' '}
                                         <Navlink text='Services'/>
                             </Link>
-                            <Link className='footerLink' to='/blog'>
-                                {' '}
-                                        <Navlink text='Blog'/>
-                            </Link>
+                           
                             <Link className='footerLink' to='/contact'>
                                 {' '}
                                         <Navlink text='Contact Me'/>
                             </Link>
                         </li>
                    </ul>
+                   </CenterContainer>
+
+                   <CenterContainer>
+                   <Link to='https://www.linkedin.com/in/laurence-moreton-burt-b5ab64a8/'>
+                   <FontAwesomeIcon className="icon" icon={faLinkedin} />
+                   </Link>
+                   <Link to='https://twitter.com/laurence_tjmb'>
+                   <FontAwesomeIcon className="icon" icon={faTwitterSquare} />
+                   </Link>
+                   <Link to='https://www.behance.net/edsear'>
+                   <FontAwesomeIcon className="icon" icon={faBehanceSquare} />
+                   </Link>
+                   
                    </CenterContainer>
 
                 <CenterContainer>
